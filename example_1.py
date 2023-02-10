6da2974bac618c52f173611f8639d4efa8e035da
Jojo <jojotime7x@gmail.com>
2023-02-10 10:49:12 +0000

    all commits

---
+++ example_1.py
@@ -0,0 +1,7 @@
+'''Problem 1: Develop an application that asks the user for her/his name, and then greets them with their name'''
+
+
+
+Name =str(input("please enter your name : "))
+
+print(f"Hello welcome {Name}")
---
+++ readme.md
@@ -0,0 +1,3 @@
+Repo details
+
+this repo is of the  lab3 problems given.
